20986194b6f0d165cda30f81e873ac2f9493d3a2
manfred-commits <manfredipiraino@gmail.com>
2021-12-26 12:46:05 +0100

    git caches clear

--- config.js
+++
@@ -1,9 +0,0 @@
-let config = {
-  emailjs: {
-    serviceID: "service_fv8254c",
-    templateID: "template_degd5hj",
-    userID: "user_iMjKxiOc7DoaZ2cZLnWHg"
-  }
-};
-
-export default config;
--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	publicPath: process.env.NODE_ENV === 'production'
-    ? '/vuejs-portofolio-basic/'
-    : '/',
-	chainWebpack: config => {
-        config.module.rules.delete('eslint');
-    }
-}
