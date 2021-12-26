77e130c8fa6726a5ce3e96a67fe5bf574adb5294
manfred-commits <manfredipiraino@gmail.com>
2021-12-26 12:55:55 +0100

    refactoring

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	publicPath: process.env.NODE_ENV === 'production'
     ? '/vuejs-portofolio-basic/'
     : '/',
+    
 	chainWebpack: config => {
         config.module.rules.delete('eslint');
     }
